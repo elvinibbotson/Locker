228a06045e1befbe4007374c7d6757cf5a599708
elvinibbotson <elvinibbotson@outlook.com>
2025-08-18 12:03:46 +0100

    Add files via upload

--- LockerScript.js
+++ LockerScript.js
@@ -51,11 +51,13 @@ function showDialog(dialog,show) {
         id(dialog).style.display='block';
         currentDialog=dialog;
         id('buttonNew').style.display='none';
+        id('curtain').style.height='100%';
     }
     else {
         id(dialog).style.display='none';
         currentDialog=null;
         id('buttonNew').style.display='block';
+        id('curtain').style.height='0';
     }
 }
 // NEW CATEGORY/NOTE
